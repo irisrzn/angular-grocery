e216c712e84b5a8865c64a0d01db0eeabfdb2885
irisrzn <iris.rizan@codern.ro>
2024-04-04 20:10:37 +0300

    added delete option and new style

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -2,28 +2,41 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-grocery',
-    templateUrl: './app.grocery.html'
+    templateUrl: './app.grocery.html',
+    styleUrls: [
+        '../assets/css/style.css'
+    ]
 })
 
 export class GroceryComponent {
-    task = {
+    item = {
         name: '',
         id: 0
     };
-    tasks: { id: number; name: string; strike: boolean; editing?: boolean;  }[] = [];
+    items: { id: number; name: string; strike: boolean; editing?: boolean; }[] = [];
 
     onClick() {
-        if (this.task.id == 0) {
-            this.tasks.push({ id: (new Date()).getTime(), name: this.task.name, strike: false });
+        if (this.item.id == 0) {
+            this.items.push({ id: (new Date()).getTime(), name: this.item.name, strike: false });
         }
+
+        this.item.name = '';
     }
 
-    onEdit(task: { id: number; name: string; strike: boolean; editing?: boolean }) {
-        task.editing = true;
+    onEdit(item: { id: number; name: string; strike: boolean; editing?: boolean }) {
+        item.editing = true;
     }
-    
+
     onSave(index: number) {
-        this.tasks[index].editing = false;
+        this.items[index].editing = false;
     }
-    
+
+    onDelete(item: { id: number; name: string; strike: boolean; editing?: boolean }) {
+        for (var i = 0; i < this.items.length; i++) {
+            if (item.id == this.items[i].id) {
+                this.items.splice(i, 1);
+                break;
+            }
+        }
+    }
 }
