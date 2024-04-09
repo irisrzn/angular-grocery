67e56f86c2aa4693779b1c34c9c42f525fc258d5
irisrzn <iris.rizan@codern.ro>
2024-04-09 15:31:13 +0300

    added measurement and quantity to data structure

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -10,20 +10,34 @@ import { Component } from '@angular/core';
 
 export class GroceryComponent {
     item = {
+        id: 0,
         name: '',
-        id: 0
+        quantity: 1,
+        measurement: 'pcs',
+        bought: false,
+        editing: false
     };
-    items: { id: number; name: string; strike: boolean; editing?: boolean; }[] = [];
+    
+    items: { id: number; name: string; quantity: number; measurement: string; bought: boolean; editing: boolean; }[] = [];
 
     onClick() {
         if (this.item.id == 0) {
-            this.items.push({ id: (new Date()).getTime(), name: this.item.name, strike: false });
+            this.items.push({ 
+                id: (new Date()).getTime(), 
+                name: this.item.name, 
+                quantity: this.item.quantity, 
+                measurement: this.item.measurement,
+                bought: false, 
+                editing: false 
+            });
         }
 
         this.item.name = '';
+        this.item.quantity = 1;
+        this.item.measurement = 'pcs';
     }
 
-    onEdit(item: { id: number; name: string; strike: boolean; editing?: boolean }) {
+    onEdit(item: { id: number; name: string; quantity: number; measurement: string; bought: boolean; editing: boolean; }) {
         item.editing = true;
     }
 
@@ -31,7 +45,7 @@ export class GroceryComponent {
         this.items[index].editing = false;
     }
 
-    onDelete(item: { id: number; name: string; strike: boolean; editing?: boolean }) {
+    onDelete(item: { id: number; name: string; quantity: number; measurement: string; bought: boolean; editing: boolean; }) {
         for (var i = 0; i < this.items.length; i++) {
             if (item.id == this.items[i].id) {
                 this.items.splice(i, 1);
@@ -40,14 +54,14 @@ export class GroceryComponent {
         }
     }
 
-    onStrike(item: { id: number; name: string; strike: boolean; editing?: boolean }) {
+    onBought(item: { id: number; name: string; bought: boolean; editing?: boolean }) {
         for (var i = 0; i < this.items.length; i++) {
             if (item.id == this.items[i].id) {
-                if (this.items[i].strike) {
-                    this.items[i].strike = false;
+                if (this.items[i].bought) {
+                    this.items[i].bought = false;
                 }
                 else {
-                    this.items[i].strike = true;
+                    this.items[i].bought = true;
                 }
                 break;
             }
---
+++ src/app/edit-item-modal/edit-item-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-item-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-item-modal.component.html',
+  styleUrl: './edit-item-modal.component.css'
+})
+export class EditItemModalComponent {
+
+}
